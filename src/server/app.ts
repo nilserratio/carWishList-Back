fb6b426c61e36a9f65cc716d623c2460d85328c8
Nil Serra Tió <nilserratio@gmail.com>
2023-10-11 20:41:45 +0200

    Install and config cors with trusted origin

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+const trustedOrigins = [process.env.ALLOWED_ORIGIN_DEV!];
+
+app.use(cors({ origin: trustedOrigins }));
 
 export default app;
