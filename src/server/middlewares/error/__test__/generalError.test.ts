b6ca67a49c9c5021462b7042c084d7125a3c1347
Nil Serra Tió <nilserratio@gmail.com>
2023-10-14 10:51:39 +0200

    Add one case of use to generalError middleware test

--- src/server/middlewares/error/__test__/generalError.test.ts
+++ src/server/middlewares/error/__test__/generalError.test.ts
@@ -1,5 +1,5 @@
 import { type Response, type Request, type NextFunction } from "express";
-import type CustomError from "../../../CustomError/CustomError.js";
+import CustomError from "../../../CustomError/CustomError.js";
 import { generalError } from "../errorMiddlewares.js";
 
 beforeEach(() => {
@@ -15,6 +15,46 @@ describe("Given a generalError middlewware", () => {
   };
 
   const next = jest.fn();
+
+  describe("When it recieve a next function with a notFoundError CustomError and a response", () => {
+    const statusCode = 404;
+    const json = "Endpoint not found";
+    const error = new CustomError(statusCode, json);
+    const expectedErrorMessage = {
+      message: json,
+    };
+
+    const req = {};
+
+    const res: Pick<Response, "status" | "json"> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    test("Then it should call the response's method with an status code 404", () => {
+      generalError(
+        error,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(error.statusCode);
+    });
+
+    test("Then it should call the response's json method with message 'Endpoint not found'", () => {
+      generalError(
+        error,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith(expectedErrorMessage);
+    });
+  });
 
   describe("When it recieve an error without status code and response", () => {
     const json = "General error, please try it in a few minutes";
