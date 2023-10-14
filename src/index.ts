2ba01011ce418a7fb64bd5acbddcc47771a6b5e4
Nil Serra Tió <nilserratio@gmail.com>
2023-10-14 12:28:26 +0200

    Connect to mongoDB database and its error handling

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import "./loadEnvironment.js";
 import createDebug from "debug";
 import chalk from "chalk";
 import app from "./server/app.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const debug = createDebug("Recomotor-api:root");
 
 const port = process.env.PORT ?? 4000;
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.red("Missing enviroment variable"));
+  process.exit(1);
+}
 
 app.listen(port, () => {
   debug(`Listening on ${chalk.green(`http://localhost:${port}`)}`);
 });
+
+try {
+  await connectToDatabase(mongoDbConnection);
+
+  debug(chalk.bgGreen("Connected to database"));
+} catch (error: unknown) {
+  debug(`Error connecting data base: ${chalk.red((error as Error).message)}`);
+}
