641892dc59bf930a1bc27fa444b5e08b9c8523d3
Nil Serra Tió <nilserratio@gmail.com>
2023-10-14 16:48:03 +0200

    Add validationError to error middleware

--- src/server/middlewares/error/errorMiddlewares.ts
+++ src/server/middlewares/error/errorMiddlewares.ts
@@ -7,6 +7,7 @@ import {
   publicMessage,
   statusCode,
 } from "../../utils/responseData/responseData.js";
+import { ValidationError } from "express-validation";
 
 const debug = createDebug("Recomotor-api:server:middleware:errorMiddlewares");
 
@@ -31,6 +32,15 @@ export const generalError = (
   _next: NextFunction
 ) => {
   debug(`Error: ${chalk.red.bold(error.message)}`);
+
+  if (error instanceof ValidationError && error.details.body) {
+    const validationError = error.details.body
+      .map((joiError) => joiError.message.replaceAll('"', ""))
+      .join(" & ");
+
+    (error as CustomError).publicMessage = validationError;
+    debug(chalk.red(validationError));
+  }
 
   const responseStatusCode = error.statusCode || statusCode.internalServerError;
 
